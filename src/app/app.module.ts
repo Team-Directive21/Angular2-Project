76d3650e21ff53e00d2eddf595044eb7f583cda4
whisper95 <mariopetrov9@gmail.com>
2016-12-28 11:36:36 +0200

    adding new components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import {  AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'register', component: RegisterComponent}
+
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    AboutComponent,
+    RegisterComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
